d6427146ad9d8ac4ced33bd3e4210a215ecdafb5
32-03\usuario <juaneliaslaiseca@hotmail.com>
2017-01-31 08:55:26 +0100

    Deshabilitada X para salir, ajustado tamaño ventana cliente, arreglado mostrar conexiones activas en tiempo real.

--- t03Tarea3Chat/src/t03tarea3chat/HiloDelServidor.java
+++ t03Tarea3Chat/src/t03tarea3chat/HiloDelServidor.java
@@ -1,58 +1,40 @@
-/*
- * Hilo que transforma en mayusculas la cadena de caractes recibida
-Termina cuando recibe el caracter *
- */
+
 package t03tarea3chat;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author juan_jesus_pedro
  */
 public class HiloDelServidor extends Thread {
 
-     DataInputStream entrada; //para flujo entrada
+    DataInputStream entrada; //para flujo entrada
     DataOutputStream salida;
-    private static final String FIN = "ADIOS"; //marca de fin de conexion
+    private static final String FIN = "*"; //marca de fin de conexion
     Socket socket = null;
-   
+
     //constructor del hilo
     public HiloDelServidor(Socket sc) {
         socket = sc;
     }
 
-//    BufferedReader entrada; //para flujo de lectura en el canal
-//    PrintWriter salida;  //para flujo de escritura en el canal
-//    Socket socket = null;
-//    ArrayList a = new ArrayList();
-//    ServidorChat sc= new ServidorChat();
-//
-//    //Constructor con el socket y los flujos de entrada y salida al canal
-//    public HiloDelServidor(Socket s) {
-//        try {
-//            socket = s;
-//            //flujos de entrada y salida
-//            entrada = new BufferedReader(new InputStreamReader(
-//                    socket.getInputStream()));
-//            salida = new PrintWriter(socket.getOutputStream(), true);
-//        } catch (IOException ex) {
-//            Logger.getLogger(HiloDelServidor.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-
     @Override
     public void run() {
-   ServidorChat.lblConexiones.setText("Num Conexiones ahora: " + ServidorChat.NumConexiones);
-        String texto = ServidorChat.jTextArea1.getText();
+        //actualizamos el número de conexiones
+        ServidorChat.lblConexiones.setText("Num Conexiones ahora: " + ServidorChat.NumConexiones);
+        //recojo el texto del servidor     
+        String texto = ServidorChat.txtServidor.getText();
+        //invoco método para crear flujo de entrada y salida
         crearFlujos();
+        //envío texto del servidor a todos los clientes
         EnvioMensajes(texto);
         boolean leer = true;
+        
+        //leo cadenas de texto, mientras no coincidan con la variable FIN
         while (leer) {
             String cadena = "";
             try {
@@ -61,13 +43,13 @@ public class HiloDelServidor extends Thread {
                 if (cadena.equals(FIN)) { // el cliente se va = marca FIN                   
                     leer = false; //deje de leer
                     ServidorChat.NumConexiones--; //decrementa la conexiones
-                    System.out.println("num "+ServidorChat.NumConexiones);
+                    System.out.println("num " + ServidorChat.NumConexiones);
                     ServidorChat.lblConexiones.setText("Num Conexiones ahora: " + ServidorChat.NumConexiones);
                     cerrarConexion(); //cierra conexion
                 } else {
                     //añado el texto del cliente al textarea del servidor
-                    ServidorChat.jTextArea1.append(cadena + "\n");
-                    texto = ServidorChat.jTextArea1.getText();
+                    ServidorChat.txtServidor.append(cadena + "\n");
+                    texto = ServidorChat.txtServidor.getText();
                     //envio contenido del texArea a todos los conectado
                     EnvioMensajes(texto);
                 }
@@ -77,10 +59,11 @@ public class HiloDelServidor extends Thread {
                 break;
             }
         }
-        
+
     }
+
     /**
-     * Obtener los flujos o canales de entrada y salida.
+     * Obtener los flujos o canales de entrada y salidaServidor.
      */
     private void crearFlujos() {
         try {            //crea el flujo entrada 
@@ -90,20 +73,19 @@ public class HiloDelServidor extends Thread {
             System.out.println("Error de entrada salida en el hilo" + ex.toString());
         }
     }
-    
-    
-     private void cerrarConexion() {
-        
+
+    private void cerrarConexion() {
+
         Iterator<Socket> it = ServidorChat.almacenSockets.iterator();
-     //recorro arraylist para localizar elemento
-        while (it.hasNext()){
-            Socket ski = (Socket)it.next();
-            if (ski==socket){
+        //recorro arraylist para localizar elemento
+        while (it.hasNext()) {
+            Socket ski = (Socket) it.next();
+            if (ski == socket) {
                 it.remove();
             }
         }
-        try { 
-         
+        try {
+
             salida.close();
             entrada.close();
             socket.close();
@@ -111,15 +93,16 @@ public class HiloDelServidor extends Thread {
             Logger.getLogger(HiloDelServidor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-private void EnvioMensajes(String texto) {
+
+    private void EnvioMensajes(String texto) {
         //recorrer ArrayList sockets para enviar los mensajes       
         System.out.println("Estoy en EnvioMensajes, numConexiones = " + ServidorChat.NumConexiones);
         for (Socket sock : ServidorChat.almacenSockets) {
-            //para cada conexión socket creo flujo salida
-            //escribo el texto en el flujo de salida
+            //para cada conexión socket creo flujo salidaServidor
+            //escribo el texto en el flujo de salidaServidor
             try {
-                DataOutputStream salida = new DataOutputStream(sock.getOutputStream());
-                salida.writeUTF(texto);
+                DataOutputStream salidaServidor = new DataOutputStream(sock.getOutputStream());
+                salidaServidor.writeUTF(texto);
             } catch (SocketException e) {
                 System.err.println("Error al intentar escribir el el socket");
             } catch (IOException ex) {
@@ -127,5 +110,5 @@ private void EnvioMensajes(String texto) {
                 System.err.println("Error de escritura");
             }
         }
-}
+    }
 }
